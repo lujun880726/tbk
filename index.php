6f4afd4d3eeb1f17900d66fbded5e916d6865219
LJ <jun.lu.726@gmail.com>
2014-07-15 16:58:12 +0800

    D

--- index.php
+++ index.php
@@ -68,13 +68,10 @@
                    
 
                 </div>
-                <div style=" position :relative;height: 300px; width: 300px;left: 30px; top:-1900px;z-index: 999">
-                    
-                    <a data-type="14" data-tmpl="300x300" data-tmplid="214" data-rd="2" data-style="2" data-border="1" href="#"></a>
-
-                </div>
+               
 				
-				<script type="text/javascript"> (function(win,doc){ var s = doc.createElement("script"), h = doc.getElementsByTagName("head")[0]; if (!win.alimamatk_show) { s.charset = "gbk"; s.async = true; s.src = "http://a.alimama.cn/tkapi.js"; h.insertBefore(s, h.firstChild); }; var o = { pid: "mm_15120310_3412872_23246093",/*推广单元ID，用于区分不同的推广渠道*/ appkey: "",/*通过TOP平台申请的appkey，设置后引导成交会关联appkey*/ unid: "<?php echo rand(0,99999999);?>"/*自定义统计字段*/ }; win.alimamatk_onload = win.alimamatk_onload || []; win.alimamatk_onload.push(o); })(window,document);</script>
+				<script type="text/javascript"> (function(win,doc){ var s = doc.createElement("script"), h = doc.getElementsByTagName("head")[0]; if (!win.alimamatk_show) { s.charset = "gbk"; s.async = true; s.src = "http://a.alimama.cn/tkapi.js"; h.insertBefore(s, h.firstChild); }; var o = { pid: "mm_15120310_3412872_23246093",/*推广单元ID，用于区分不同的推广渠道*/ appkey: "",/*通过TOP平台申请的appkey，设置后引导成交会关联appkey*/ unid: ""/*自定义统计字段*/ }; win.alimamatk_onload = win.alimamatk_onload || []; win.alimamatk_onload.push(o); })(window,document);</script>
+
 
             </div>
 
